2ebd4c4581d78a31b738d9261cac1f590b8e5995
zdb17173 <zdb17173@163.com>
2018-02-01 17:23:42 +0800

    json异常捕获

--- front-springboot/src/main/java/org/fran/front/springboot/controller/ExceptionController.java
+++ front-springboot/src/main/java/org/fran/front/springboot/controller/ExceptionController.java
@@ -1,11 +1,13 @@
 package org.fran.front.springboot.controller;
 
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.fran.front.springboot.vo.JsonResult;
 import org.springframework.boot.autoconfigure.web.BasicErrorController;
 import org.springframework.boot.autoconfigure.web.ErrorAttributes;
 import org.springframework.boot.autoconfigure.web.ErrorProperties;
 import org.springframework.boot.autoconfigure.web.ErrorViewResolver;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -21,15 +23,25 @@ import java.util.Map;
 /**
  * Created by fran on 2018/1/22.
  */
-@Controller
-public class ErrorController implements ErrorViewResolver {
+@ControllerAdvice
+public class ExceptionController implements ErrorViewResolver {
+
+    @ResponseBody
+    @ExceptionHandler(value=Exception.class)
+    public JsonResult allExceptionHandler(
+            HttpServletRequest request, Exception exception) throws Exception
+    {
+        JsonResult r = new JsonResult();
+        r.setDescription(exception.getMessage());
+        r.setStatus(500);
+        return r;
+    }
 
     @Override
     public ModelAndView resolveErrorView(
-            javax.servlet.http.HttpServletRequest httpServletRequest,
+            javax.servlet.http.HttpServletRequest request,
             HttpStatus httpStatus,
             Map<String, Object> map) {
-
         return new ModelAndView("/error", map);
     }
 }
--- front-springboot/src/main/java/org/fran/front/springboot/controller/FreeMarkerController.java
+++ front-springboot/src/main/java/org/fran/front/springboot/controller/FreeMarkerController.java
@@ -2,16 +2,14 @@ package org.fran.front.springboot.controller;
 
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.fran.front.springboot.security.FreemarkerUtil;
+import org.fran.front.springboot.vo.JsonResult;
 import org.fran.front.springboot.vo.UserVo;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.util.WebUtils;
 
@@ -25,6 +23,27 @@ import java.util.*;
 @Controller
 @RequestMapping("/local")
 public class FreeMarkerController {
+
+    @ResponseBody
+    @GetMapping(value = "/selectList", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.ALL_VALUE)
+    public JsonResult<List<String>> selectTest(@RequestParam(name="id") int ids){
+        if(ids == 5)
+            throw new RuntimeException("error!!!!");
+
+        JsonResult<List<String>> res = new JsonResult<>();
+        res.setData(new ArrayList<String>(){
+            {
+                add("sadsda1");
+                add("sadsda2");
+                add("sadsda3");
+                add("sadsda4");
+                add("sadsda5");
+            }
+        });
+        res.setDescription("sahdjhsajdhjsahdjsajdjh");
+        res.setStatus(200);
+        return res;
+    }
 
     @RequestMapping("/{template}")
     public ModelAndView selectTest(
